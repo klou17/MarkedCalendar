8dc080c00a0e338160c1df7b8ce8fa1ed576bc6e
klou17 <nikollazarte04@gmail.com>
2024-03-21 22:29:51 -0500

    feature: create project

---
+++ MarkedCalendar/MarkedCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkedCalendarApp.swift
+//  MarkedCalendar
+//
+//  Created by Nikol Lazarte on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MarkedCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
